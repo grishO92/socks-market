d3ad44a6fa881c659e383fc8cd801cc1e672e6a9
grishO92 <grigoryutta@gmail.com>
2022-05-03 20:50:14 +0200

    initial setup and Nav component

---
+++ src/components/header/Header.js
@@ -0,0 +1,27 @@
+import { NavLink, Link } from 'react-router-dom';
+import './Header.css';
+
+export const Header = () => {
+  return (
+    <nav className="nav">
+      <Link className="nav-btn logo" to="/">
+        socksMarket
+      </Link>
+
+      <section className="nav-right">
+        <NavLink className="nav-btn catalog" to="/">
+          Catalog
+        </NavLink>
+        <NavLink className="nav-btn login" to="login">
+          Login
+        </NavLink>
+        <NavLink className="nav-btn register" to="register">
+          Register
+        </NavLink>
+        <button className="nav-btn profile" to="profile">
+          Usr
+        </button>
+      </section>
+    </nav>
+  );
+};
