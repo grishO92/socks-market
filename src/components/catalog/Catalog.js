ce2c7d6749fc3113b82492a0e0f7900d892785df
grishO92 <grigoryutta@gmail.com>
2022-05-12 12:41:02 +0200

    added categories styling

--- src/components/catalog/Catalog.js
+++ src/components/catalog/Catalog.js
@@ -4,12 +4,14 @@ export const Catalog = () => {
   return (
     <section className="catalog">
       <h1 className="title">Catalog</h1>
-      <section className="categories">
-        <article className="men">men</article>
-        <article className="women">women</article>
-        <article className="boys">boys</article>
-        <article className="girls">girls</article>
-      </section>
+      <article className="wrapper-category">
+        <section className="categories">
+          <button className="category-btn men">men</button>
+          <button className="category-btn women">women</button>
+          <button className="category-btn boys">boys</button>
+          <button className="category-btn girls">girls</button>
+        </section>
+      </article>
     </section>
   );
 };
